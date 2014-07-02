13637344e2a4274479e0c2de0ee33aca4d958f8a
Jennifer Turner <jenniferturner202@gmail.com>
2014-07-02 09:13:18 -0400

    Added User Entry function

--- js/app.js
+++ js/app.js
@@ -1,17 +1,75 @@
 
 $(document).ready(function(){
+
+var eachGuess = function() {
+	var entry = $('#eachGuess').val();
+	$('#guessList').append('<li>'+entry+'</li>');
+	$('#userGuess').val('');
+	//$('entry').append('#guesslist');
+	event.preventDefault();
+};
+
+//'<li><input type="text" id="userGuess" class="guessBox clearfix" ::before/>'+entry+'</li>'
+
+	//NEW GAME FUNCTION 
+	//var newGame = function() {
+	//function newGame() {
+	/*$('.new').click(function(){
+	var entry = $('#userGuess').val();
+	$('#guessList').append('<li><input type="text" id="userGuess" class="guessBox clearfix"/>'+entry+'</li>');
+	$('#userGuess').val('');
+	$('entry').append('#guesslist');
+	event.preventDefault();
+});
+	
+});*/
+
+
+/*
+**SECRET NUMBER FUNCTION
+	var secretNumber = function(x) {
+		var randomNumber = Math.floor(Math.random()*100)+1);
+		if ('#userGuess' === randomNumber)
+		return x;
+		} else {
+		alert("Enter a number between 1 and 100");
+		}
+	
+	
+	}
+
+**USER GUESS FUNCTION */
+
+$(function(){
+	$('form').on('submit', eachGuess);
+});
+});
+
+/*
+	$('#userGuess').on('submit') {
+	$('#feedback').append()
+	}
+	
+
+	}
+	*/
+
+	/*
+}
+
+	*/ 
 	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
+  /*	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
   	});
 
   	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+  /*	$("a.close").click(function(){ */
+  		//$(".overlay").fadeOut(1000);
+  //	}); 
 
-});
-
-
+//});
+//*
+//}
