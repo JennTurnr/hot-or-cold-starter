802e59e7501ca8d4a7f80f3c93f06af312c3965b
Jennifer Turner <jenniferturner202@gmail.com>
2014-07-06 09:11:07 -0400

    Added comments for other functions

--- js/app.js
+++ js/app.js
@@ -1,53 +1,63 @@
-
 $(document).ready(function(){
-	//var entry = $('eachGuess').val();
+
+	//var count = 0;
+	//var abs = Math.abs(x);
+	//var i = Math.floor((Math.random()*100) +1);
+	
 
 var eachGuess = function() {
 	var entry = $('#userGuess').val();
 	//console.log( 'starting eachGuess()');
 	$('#guessList').append('<li>'+entry+'</li>');
 	$('#userGuess').val('');
-	$('li').css('border', '2px solid yellow');
+	//$('li').css('border', '2px solid yellow');
 	//$('entry').append('#guesslist');
 	event.preventDefault();
 };
 
-//'<li><input type="text" id="userGuess" class="guessBox clearfix" ::before/>'+entry+'</li>'
-
-	//NEW GAME FUNCTION 
-	//var newGame = function() {
-	//function newGame() {
-	/*$('.new').click(function(){
-	var entry = $('#userGuess').val();
-	$('#guessList').append('<li><input type="text" id="userGuess" class="guessBox clearfix"/>'+entry+'</li>');
-	$('#userGuess').val('');
-	$('entry').append('#guesslist');
-	event.preventDefault();
-});
-	
-});*/
+//Keeps track of the number of guesses per game
+	//var counter = $('#guesslist').children.length();
+	//$('#count').text(counter); //span div
+	//event.preventDefault();
 
 
-/*
-**SECRET NUMBER FUNCTION
-	var secretNumber = function(x) {
-		var randomNumber = Math.floor(Math.random()*100)+1);
-		if ('#userGuess' === randomNumber)
-		return x;
-		} else {
+//var secretNumber = function() {
+	//if (entry >= 101 || entry <=0 || entry%1 !=0 || isNaN(entry)) {
+	//	alert("Enter a number between 1 and 100!");
+	//	$('#count').text
+		/*return i;
+	} else {
 		alert("Enter a number between 1 and 100");
-		}
-	
-	
 	}
+	if (i >= 50){
+		return $('#feedback').append('<p>You're ice cold</p>');
+	} else if (i <=50 && i >= 30) {
+		return $('#feedback').append("You're getting warm.");
+	} else if (x <=30 && x >= 20) {
+		return $('#feedback').append("You're getting hot.");
+	} else if (i < 20) {
+		return i;
+	}
+	secretNumber();
 
-**USER GUESS FUNCTION */
 
-$(function(){
+
+	//**NEW GAME FUNCTION **
+	//function newGame () {
+	//	$(entry).remove();
+	//}
+	//var newGame = function() {
+	//	$('#  
+
+//}
+
+
+
+$(function() {
 	$('form').on('submit', eachGuess);
+	//$('.new').on('click', newGame); 
 });
 });
-
 /*
 	$('#userGuess').on('submit') {
 	$('#feedback').append()
@@ -57,14 +67,29 @@ $(function(){
 	}
 	*/
 
-	/*
+/*
 }
+//*
+//}
 
-	*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 	/*--- Display information modal box ---*/
   /*	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+    //	$(".overlay").fadeIn(1000);
 
   	});
 
@@ -74,5 +99,4 @@ $(function(){
   //	}); 
 
 //});
-//*
-//}
+//*}
